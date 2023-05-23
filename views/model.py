8c8099050879a160918724bc11bb7f82a18fb4dd
khaled boussaidi <khaled.boussaidi@codeonce.fr>
2023-05-23 21:20:27 +0100

    ML

---
+++ views/model.py
@@ -0,0 +1,89 @@
+from flask import Flask,request
+from flask_mongoengine import MongoEngine
+from werkzeug.utils import secure_filename
+import os,os.path
+from models.models import Workspace
+from models.models import Models
+from flask_swagger_ui import get_swaggerui_blueprint
+import h2o
+from h2o.automl import H2OAutoML
+from models.models import File
+
+
+app = Flask(__name__)
+## swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Workspaces_H2O_Flask-REST-Api"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+app.config['MONGODB_SETTINGS'] = {
+    'db':'db_name',
+    'host':'localhost',
+    'port':'27017'
+}
+
+
+app.config['MONGODB_SETTINGS'] = {
+    'host':'mongodb://localhost:27017'}
+db=MongoEngine(app)
+@app.route('/models', methods=['GET'])  
+def get_models():  
+        obj=Models()
+        return (obj.read())
+@app.route('/models/<id>', methods=['DELETE'])
+def delete_model(id):
+    obj=Models()
+    return (obj.Delete(id))
+
+@app.route('/workspaces/models/<id>', methods=['POST']) #add a model  by id of workspace
+def add_model(id):    
+        workspace = Workspace.objects.get_or_404(id=id)
+        m=Models()
+        fd= request.files['file']
+        m.model_name=fd.filename
+        m.path=os.path.join(os.path.abspath('../ML_models'), fd.filename)
+        filename = secure_filename(fd.filename)
+        fd.save( filename)
+        m.save()
+        workspace.models.append(m)
+        workspace.save()
+        return("model added successefully")
+
+
+@app.route('/train/<id>', methods=['POST'])
+def apload(id):
+    h2o.init()
+    json_data = request.json
+    a_value = json_data["a_key"]
+    file = File.objects.get_or_404(id=id)
+    data = h2o.import_file(path=file.path, destination_frame="train")
+    train, test = data.split_frame(ratios=[0.8])
+    x = train.columns
+    print(x)
+    y = a_value
+    x.remove(y)
+    train[y] = train[y].asfactor()
+    test[y] = test[y].asfactor()
+    aml = H2OAutoML(max_models=20, seed=1)
+    aml.train(x=x, y=y, training_frame=train)
+    lb = aml.leaderboard
+
+    model_ids = list(lb['model_id'].as_data_frame().iloc[:, 0])
+    out_path = "../ML_models"
+    for m_id in model_ids: # save all models locally
+        mdl = h2o.get_model(m_id)
+        h2o.save_model(model=mdl, path=out_path, force=True)
+    h2o.export_file(lb, os.path.join(out_path, file.filename + 'algorithms'), force=True)
+    lbdf = lb.head(rows=10).as_data_frame()
+    js = lbdf.to_json()
+    return (js) # return all the models version with their performance
+
+if __name__ == "__main__":
+    app.run(port=5005, debug=True)
---
+++ views/predict.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request, Response,json,render_template
+from flask_mongoengine import MongoEngine
+from mongoengine import *
+from werkzeug.utils import secure_filename
+import os,os.path
+from bson.dbref import DBRef
+from datetime import datetime
+from h2o.automl import H2OAutoML
+from flask_swagger_ui import get_swaggerui_blueprint
+import h2o
+from h2o.automl import H2OAutoML
+
+
+app = Flask(__name__)
+## swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger_predict.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Workspaces_H2O_Flask-REST-Api"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+@app.route('/predict/<model>',methods=['POST'])    # prediction by choosing the model key
+def predict(model):
+         h2o.init()
+         item_dict = {}
+         model_name = os.path.join("./ML_models", model)
+         uploaded_model = h2o.load_model(model_name)   
+         testing= request.get_json() 
+         test = h2o.H2OFrame(testing)
+         pred_ans = uploaded_model.predict(test).as_data_frame()
+         item_dict['Prediction'] = int(pred_ans.predict.values[0])
+         print(item_dict)
+         return (item_dict)    # return the result /the prediction
+        
+if __name__ == "__main__":
+    app.run(debug=True, port=5004)
